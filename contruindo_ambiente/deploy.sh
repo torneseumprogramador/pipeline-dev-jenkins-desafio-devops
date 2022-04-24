fd64e7370db59b5fcaf51c064c1ec2929ff9fa70
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-04-24 05:23:28 -0300

    config pipeline dev

--- contruindo_ambiente/deploy.sh
+++ contruindo_ambiente/deploy.sh
@@ -11,7 +11,7 @@ echo "[ec2]" > ../1-ansible/hosts # cria arquivo
 echo "$(terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
-sleep 20 # 20 segundos
+sleep 30 # 20 segundos
 
 cd ../1-ansible
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /home/ubuntu/id_rsa
